551db9ac785a8670175d062e0e7d85c27491bc3e
MikkiSeidenschnur <mikki.seidenschnur@gmail.com>
2021-10-08 14:42:17 +0200

    Add space classes to map spaces with bounding box

---
+++ HVACExporter/Models/Spaces/Space.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Revit.DB;
+using System;
+
+namespace HVACExporter.Models.Spaces
+{
+    public class Space
+    {
+        public string Id { get; set; }
+        public string Tag { get; set; }
+        public double HeatingDemand { get; set; }
+        public SpaceBoundingBox SpaceBoundingBox { get; set; }
+
+        public Space(string id, string tag, double heatingDemand, SpaceBoundingBox spaceBoundingBox)
+        {
+            Id = id;
+            Tag = tag;
+            HeatingDemand = heatingDemand;
+            SpaceBoundingBox = spaceBoundingBox;
+        }
+
+        public void GetContainedMechanicalElements(Element space)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
