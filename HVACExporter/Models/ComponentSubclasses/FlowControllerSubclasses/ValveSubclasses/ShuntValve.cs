de3c1c9379be07eb926d7f086f51b6c1ab4078ac
MikkiSeidenschnur <mikki.seidenschnur@gmail.com>
2021-10-13 15:21:46 +0200

    Adding ShuntValve class to hierarchy

---
+++ HVACExporter/Models/ComponentSubclasses/FlowControllerSubclasses/ValveSubclasses/ShuntValve.cs
@@ -0,0 +1,17 @@
+﻿namespace HVACExporter.Models.ComponentSubclasses.FlowControllerSubclasses.ValveSubclasses
+{
+    public class ShuntValve : Valve
+    {
+        public double ShuntDiameter { get; set; }
+        public double HasCheckValve { get; set; }
+
+        public ShuntValve(string id, string tag, string systemName, string systemType,
+                              double shuntDiameter, double hasCheckValve)
+            : base(id, tag, systemName, systemType)
+        {
+            ShuntDiameter = shuntDiameter;
+            HasCheckValve = hasCheckValve;
+            ComponentType = this.GetType().Name;
+        }
+    }
+}
