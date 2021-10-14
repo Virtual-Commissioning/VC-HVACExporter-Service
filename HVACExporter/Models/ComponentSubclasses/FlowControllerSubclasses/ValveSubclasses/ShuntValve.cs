e3e63534c1d7e43e83a6bee22b7e91978946616b
efjer <eusb@live.dk>
2021-10-14 11:58:45 +0200

    Changing hasCheckValve to boolean instead of double

--- HVACExporter/Models/ComponentSubclasses/FlowControllerSubclasses/ValveSubclasses/ShuntValve.cs
+++ HVACExporter/Models/ComponentSubclasses/FlowControllerSubclasses/ValveSubclasses/ShuntValve.cs
@@ -3,10 +3,10 @@
     public class ShuntValve : Valve
     {
         public double ShuntDiameter { get; set; }
-        public double HasCheckValve { get; set; }
+        public bool HasCheckValve { get; set; }
 
         public ShuntValve(string id, string tag, string systemName, string systemType,
-                              double shuntDiameter, double hasCheckValve)
+                              double shuntDiameter, bool hasCheckValve)
             : base(id, tag, systemName, systemType)
         {
             ShuntDiameter = shuntDiameter;
