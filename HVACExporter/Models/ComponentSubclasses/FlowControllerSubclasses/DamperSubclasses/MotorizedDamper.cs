f6a7f8417af2d2b537da47d6ecfaef04cb92beea
MikkiSeidenschnur <mikki.seidenschnur@gmail.com>
2021-10-20 14:54:02 +0200

    Added control attribute to MotorizedDamper and MotorizedValve

--- HVACExporter/Models/ComponentSubclasses/FlowControllerSubclasses/DamperSubclasses/MotorizedDamper.cs
+++ HVACExporter/Models/ComponentSubclasses/FlowControllerSubclasses/DamperSubclasses/MotorizedDamper.cs
@@ -1,15 +1,19 @@
-﻿namespace HVACExporter.Models.ComponentSubclasses.FlowControllerSubclasses.DamperSubclasses
+﻿using HVACExporter.Models.Controls;
+
+namespace HVACExporter.Models.ComponentSubclasses.FlowControllerSubclasses.DamperSubclasses
 {
     class MotorizedDamper : Damper
     {
         public double Kv { get; set; }
         public double Kvs { get; set; }
+        public Controller Control { get; set; }
 
-        public MotorizedDamper(string id, string tag, string systemName, string systemType, double kv, double kvs)
+        public MotorizedDamper(string id, string tag, string systemName, string systemType, double kv, double kvs, Controller control)
             : base(id, tag, systemName, systemType)
         {
             Kv = kv;
             Kvs = kvs;
+            Control = control;
             ComponentType = this.GetType().Name;
         }
     }
