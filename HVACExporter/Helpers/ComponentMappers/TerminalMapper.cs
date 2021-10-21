898e9379b277b5e571a07567cbd537e2af83b502
efjerb <78088793+efjerb@users.noreply.github.com>
2021-10-21 10:37:12 +0200

    Add mapper for air terminals (#41)

---
+++ HVACExporter/Helpers/ComponentMappers/TerminalMapper.cs
@@ -0,0 +1,30 @@
+﻿using Autodesk.Revit.DB;
+using HVACExporter.Models.ComponentSubclasses.TerminalSubclasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HVACExporter.Helpers.ComponentMappers
+{
+    class TerminalMapper
+    {
+        public static AirTerminal MapToAirTerminal(MEPModel terminal)
+        {
+            string id = terminal.ConnectorManager.Owner.UniqueId;
+            string tag = terminal.ConnectorManager.Owner.Id.ToString();
+            string systemIdentifiers = terminal.ConnectorManager.Owner.LookupParameter("System Type").AsValueString().ToLower();
+            string systemType = HelperFunctions.GetSystemType(systemIdentifiers);
+
+            double kv = terminal.ConnectorManager.Owner.LookupParameter("FSC_kv").AsDouble();
+
+            AirTerminal component = new AirTerminal(id, tag, systemIdentifiers, systemType, kv);
+
+            component.FillConnectedComponents(terminal);
+
+            return component;
+        }
+        
+    }
+}
