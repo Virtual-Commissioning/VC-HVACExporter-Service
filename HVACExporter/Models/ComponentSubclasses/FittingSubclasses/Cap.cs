61e77ed3dcee9417021617781664f3ac67dd5e1c
MikkiSeidenschnur <mikki.seidenschnur@gmail.com>
2022-01-31 16:24:54 +0100

    Added Cap class

---
+++ HVACExporter/Models/ComponentSubclasses/FittingSubclasses/Cap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HVACExporter.Models.ComponentSubclasses.FittingSubclasses
+{
+    class Cap
+    {
+        class Cap : Fitting
+        {
+            public Cap(string id, string tag, string systemName, string systemType)
+                : base(id, tag, systemName, systemType)
+            {
+                ComponentType = this.GetType().Name;
+            }
+        }
+    }
+}
