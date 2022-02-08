cdaa712d3601657f2f76b926de6da17b253d122d
JonTangeraas <jon-martin97@hotmail.com>
2022-02-08 10:02:28 +0100

    Added HVAC Class

---
+++ HVACExporter/Models/Spaces/Zone/HVAC/Thermostat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HVACExporter.Models.Thermostat
+{
+    public class Thermostat
+    {
+        public string Id { get; set; }
+        public string Tag { get; set; }
+        public string HeatingSetpointSchedule { get; set; }
+        public string CoolingSetpointSchedule { get; set; }
+
+        public Thermostat(string id, string tag, string heatingSetpointSchedule, string coolingSetpointSchedule)
+        {
+            Id = id;
+            Tag = tag;
+            HeatingSetpointSchedule = heatingSetpointSchedule;
+            CoolingSetpointSchedule = coolingSetpointSchedule;
+        }
+    }
+}
