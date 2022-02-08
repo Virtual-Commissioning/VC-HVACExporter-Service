0576cdd3fbcbab6e807c9eb377da23e9d422ef28
JonTangeraas <jon-martin97@hotmail.com>
2022-02-08 10:03:20 +0100

    Added Site Class

---
+++ HVACExporter/Models/Spaces/Zone/Site/Site.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HVACExporter.Models.Site
+{
+    public class Site
+    {
+        public string SiteId { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string TimeZone { get; set; }
+        public double Elevation { get; set; }
+
+        public Site(string siteId, double latitude, double longitude, string timeZone, double elevation)
+        {
+            SiteId = siteId;
+            Latitude = latitude;
+            Longitude = longitude;
+            TimeZone = timeZone;
+            Elevation = elevation;
+        }
+    }
+}
