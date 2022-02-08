3cd489bfbc11495d08de7175d1b3064310fe1e81
JonTangeraas <jon-martin97@hotmail.com>
2022-02-08 10:02:51 +0100

    Added InternalGains Class

---
+++ HVACExporter/Models/Spaces/Zone/InternalGains/Equiptment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HVACExporter.Models.Equiptment
+{
+    public class Equiptment
+    {
+        public string Id { get; set; }
+        public string Tag { get; set; }
+        public string LightingSchedule { get; set; }
+        public string CalculationMethod { get; set; }
+        public double LightingLevel { get; set; }
+        public double WattM2 { get; set; }
+        public double ReturnAirFraction { get; set; }
+        public double FractionRadiant { get; set; }
+        public double FractionLost { get; set; }
+        public string EndUseSubCategory { get; set; }
+
+        public Equiptment(string id, string tag, string lightingSchedule, CalculationMethod calculationMethod, double lightingLevel, double wattM2, double returnAirFraction, double fractionRadiant, double fractionLost, string endUseSubCategory)
+        {
+            Id = id;
+            Tag = tag;
+            LightingSchedule = lightingSchedule;
+            CalculationMethod = calculationMethod;
+            LightingLevel = lightingLevel;
+            WattM2 = wattM2;
+            ReturnAirFraction = returnAirFraction;
+            FractionRadiant = fractionRadiant;
+            FractionVisible = fractionLost;
+            EndUseSubCategory = endUseSubCategory;
+        }
+    }
+}
---
+++ HVACExporter/Models/Spaces/Zone/InternalGains/Lighting.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HVACExporter.Models.Lighting
+{
+    public class Lighting
+    {
+        public string Id { get; set; }
+        public string Tag { get; set; }
+        public string LightingSchedule { get; set; }
+        public string CalculationMethod { get; set; }
+        public double LightingLevel { get; set; }
+        public double WattM2 { get; set; }
+        public double ReturnAirFraction { get; set; }
+        public double FractionRadiant { get; set; }
+        public double FractionVisible { get; set; }
+        public double FractionReplaceable { get; set; }
+        public string EndUseSubCategory { get; set; }
+
+        public Lighting(string id, string tag, string lightingSchedule, CalculationMethod calculationMethod, double lightingLevel, double wattM2, double returnAirFraction, double fractionRadiant, double fractionVisible, double fractionReplaceable, string endUseSubCategory)
+        {
+            Id = id;
+            Tag = tag;
+            LightingSchedule = lightingSchedule;
+            CalculationMethod = calculationMethod;
+            LightingLevel = lightingLevel;
+            WattM2 = wattM2;
+            ReturnAirFraction = returnAirFraction;
+            FractionRadiant = fractionRadiant;
+            FractionVisible = fractionVisible;
+            FractionReplaceable = fractionReplaceable;
+            EndUseSubCategory = endUseSubCategory;
+        }
+    }
+}
