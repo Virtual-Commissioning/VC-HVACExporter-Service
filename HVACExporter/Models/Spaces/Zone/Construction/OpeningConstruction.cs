9b7dfbf94f6c8cc043c91652f53dbab421b869ed
JonTangeraas <jon-martin97@hotmail.com>
2022-02-08 10:02:13 +0100

    Added Construction Class

---
+++ HVACExporter/Models/Spaces/Zone/Construction/OpeningConstruction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HVACExporter.Models.OpeningConstruction
+{
+    public class OpeningConstruction
+    {
+        public string Id { get; set; }
+        public string Tag { get; set; }
+        public string AirExchangeMethod { get; set; }
+        public double SimpleMixingACPrHour { get; set; }
+
+        public OpeningConstruction(string id, string tag, string airExchangeMethod, double simpleMixingPrHour)
+        {
+            Id = id;
+            Tag = tag;
+            AirExchangeMethod = airExchangeMethod;
+            SimpleMixingACPrHour = simpleMixingPrHour;
+        }
+    }
+}
