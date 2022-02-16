256eca79d25f2cbfe311f2e4e3e4e349eaa6e7cb
JonTangeraas <jon-martin97@hotmail.com>
2022-02-16 16:11:41 +0100

    Merge of USA01 into USA02

--- HVACExporter/Models/Spaces/Zone/Zone.cs
+++ HVACExporter/Models/Spaces/Zone/Zone.cs
@@ -10,7 +10,8 @@ namespace HVACExporter.Models.Zone
     {
         public string Id { get; set; }
         public string Tag { get; set; }
-        public Coordinates XYZOrigin { get; set; } // Alternatly use the class XYZOrigin that was created.
+        //public Coordinate Point { get; set; } // Alternatly use the class XYZOrigin that was created.
+        public GeometricTypes.Coordinate Point { get; set; }
         public string ZoneType { get; set; }
         public double CeilingHeight { get; set; }
         public double FloorArea { get; set; }
@@ -18,19 +19,18 @@ namespace HVACExporter.Models.Zone
         public string IntConvAlg { get; set; }
         public string OutConvAlg { get; set; }
         public bool IncludedInTotArea { get; set; }
-
-        public Zone(string id, string tag, XYZOrigin xyzOrigin, double ceilingHeight, double floorArea, double zoneVolume, string intConvAlg, string outConvAlg, bool includedInTotArea)
+        
+        public Zone(string id, string tag, GeometricTypes.Coordinate point, string zoneType, double ceilingHeight, double floorArea, double zoneVolume, string intConvAlg, string outConvAlg, string includedInTotArea)
         {
             Id = id;
             Tag = tag;
-            XYZOrigin = xyzOrigin;
+            this.Point = point;
+            ZoneType = zoneType;
             CeilingHeight = ceilingHeight;
             FloorArea = floorArea;
             ZoneVolume = zoneVolume;
             IntConvAlg = intConvAlg;
             OutConvAlg = outConvAlg;
-            IncludedInTotArea = includedInTotArea;
         }
-
     }
 }
