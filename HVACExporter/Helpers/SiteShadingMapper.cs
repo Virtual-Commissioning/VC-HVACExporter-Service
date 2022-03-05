b794128190496d94e5dbb9447087d56d0b3d59c3
JonTangeraas <jon-martin97@hotmail.com>
2022-03-05 16:31:14 +0100

    Defined Shading Mapper for zone and site

---
+++ HVACExporter/Helpers/SiteShadingMapper.cs
@@ -0,0 +1,17 @@
+﻿using HVACExporter.Models.Zone;
+
+namespace HVACExporter.Helpers.ZoneMappers.InternalGainsMappers
+{
+    public class SiteShadingMapper
+    {
+        public static ShadingSite MapSiteShading(Autodesk.Revit.DB.Mechanical.Space associatedSpace)
+        {
+            string id = "Shading_Site";
+            string transmSchedule = "NA";
+            VertexCoordinates vertexCoordinates = null;
+
+            ShadingSite shadingSite = new ShadingSite(id, transmSchedule, vertexCoordinates);
+            return shadingSite;
+        }
+    }
+}
